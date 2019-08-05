b6ff0a98444585fe9d68bd034cc180908a141899
naufal-dean <naufal.dean@gmail.com>
2019-08-06 00:18:27 +0700

    Initial local commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="{{url('/index')}}">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{url('/about')}}">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="{{url('/services')}}">Services</a>
+    </li>
+  </ul>
+</nav>
